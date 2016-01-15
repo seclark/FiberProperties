33b678ae5efc2b0e94f6dee3981d1b113697cce5
Susan Clark <susanclark19@gmail.com>
2016-01-14 23:49:51 -0500

    testing

--- code/extract.py
+++ code/extract.py
@@ -1,6 +1,8 @@
 from __future__ import division, print_function
 import numpy as np
 from astropy.io import fits
+from scipy.ndimage.morphology import grey_erosion, grey_dilation
+import matplotlib.pyplot as plt
 
 # RHT helper code
 import sys 
@@ -12,6 +14,7 @@ def get_data(verbose = True):
     This can be rewritten for whatever data we're using.
     Currently just grabs some of the SC_241 data from the PRL.
     """
+    
     root = "/Users/susanclark/Dropbox/GALFA-Planck/Big_Files/"
     data_fn = root + "SC_241.66_28.675.best_20_xyt_w75_s15_t70_galfapixcorr.fits"
     data = fits.getdata(data_fn)
@@ -22,24 +25,87 @@ def get_data(verbose = True):
         
     return data, data_fn
 
-# Load GALFA-HI RHT data
-galfa, galfa_fn = get_data()
-
-# Separate into indices and R(theta) arrays
-ipoints, jpoints, rthetas, naxis1, naxis2 = RHT_tools.get_RHT_data(galfa_fn)
-npoints, nthetas = rthetas.shape
-print("There are %d theta bins" %nthetas)
-
-# Number of theta bins to separate RHT data into 
-nbins = 10
-
-cube_thetas = np.zeros((naxis1, naxis2, nthetas), np.float_)
-cube_thetas[ipoints, jpoints, :] = rthetas
-theta_separated_backprojection = np.zeros((naxis1, naxis2, nbins), np.float_)
-for i in xrange(nbins):
-    theta_separated_backprojection[:, :, i] = np.sum(cube_thetas[:, :, i*nbins:(i+1)*nbins], axis=2)
+def bin_data_by_theta(nbins = 10):
+    """
+    Places RHT data into cube binned by theta. 
     
+    Input:
+    nbins :: number of theta bins to separate RHT data into (default = 10)
+    
+    Output:
+    theta_separated_backprojection :: 3D array, dimensions (naxis1, naxis2, nbins).
+                                   :: contains backprojection summed into nbins chunks.
+    """
+    
+    # Load GALFA-HI RHT data
+    galfa, galfa_fn = get_data()
 
+    # Separate into indices and R(theta) arrays
+    ipoints, jpoints, rthetas, naxis1, naxis2 = RHT_tools.get_RHT_data(galfa_fn)
+    npoints, nthetas = rthetas.shape
+    print("There are %d theta bins" %nthetas)
+
+    cube_thetas = np.zeros((naxis2, naxis1, nthetas), np.float_)
+    cube_thetas[jpoints, ipoints, :] = rthetas
+    theta_separated_backprojection = np.zeros((naxis2, naxis1, nbins), np.float_)
+    for i in xrange(nbins):
+        theta_separated_backprojection[:, :, i] = np.sum(cube_thetas[:, :, i*nbins:(i+1)*nbins], axis=2)
+        
+    return theta_separated_backprojection
+
+def erode_data(data, footprint = None):
+    
+    if footprint != None:
+        eroded_data = grey_erosion(data, footprint = footprint)
+    else:
+        eroded_data = grey_erosion(data, size=(10, 10))
+    
+    return eroded_data
+    
+def dilate_data(data, footprint = None):
+    
+    if footprint != None:
+        dilated_data = grey_dilation(data, footprint = footprint)
+    else:
+        dilated_data = grey_dilation(data, size=(10, 10))
+    
+    return dilated_data
+    
+def make_footprint(radius = 10):
+    """
+    Make a footprint of a given radius (in pixels) for use in erosion/dilation.
+    """
+    
+    fp = np.zeros((2*radius+1, 2*radius+1), np.float_)
+    mnvals = np.indices(fp.shape)
+    mvals = mnvals[:, :][0] # These are the y points
+    nvals = mnvals[:, :][1] # These are the x points
+    rads = np.zeros(fp.shape, np.float_)
+    rads = np.sqrt((mvals-radius)**2 + (nvals-radius)**2)
+    
+    fp[rads < radius] = 1
+    
+    return fp
+    
+    
+theta_separated_backprojection = bin_data_by_theta(nbins = 10)
+
+# Scale to [0, 1]
+theta_separated_backprojection = theta_separated_backprojection/np.nanmax(theta_separated_backprojection)
+footprint = make_footprint(radius = 30)
+eroded_theta4 = erode_data(theta_separated_backprojection[:, :, 4], footprint = footprint)
+dilated_theta4 = dilate_data(eroded_theta4, footprint = footprint)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312)
+ax3 = fig.add_subplot(313)
+
+ax1.imshow(theta_separated_backprojection[:, :, 4])
+ax2.imshow(eroded_theta4)
+ax3.imshow(dilated_theta4)
+
+    
 
     
 
